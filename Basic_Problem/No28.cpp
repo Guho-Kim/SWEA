c24679fa0f14d1a25ef8ba7cde6a4027af58d9a0
Guho-Kim <weeeeek2@naver.com>
2023-08-02 01:27:00 +0900

    2023-08-02

--- Basic_Problem/No28.cpp
+++ Basic_Problem/No28.cpp
@@ -17,7 +17,13 @@ int solve(){
     
     for(int i=1; i<=N; ++i){
         for(int j=1; j<=M; ++j){
-            dp[i][j]=max(dp[i-1][j], A[i]+dp[i-1][j]);
+            for(int k=1; k<=j; k++){
+                /*
+                    j개 B 과자를 넣을 때, max를 찾기 위해 for loop을 돌아야 함.
+                */
+                dp[i][j]=max(dp[i-1][j], A[i]+dp[i-1][j]);
+
+            }
         }
     }
     
