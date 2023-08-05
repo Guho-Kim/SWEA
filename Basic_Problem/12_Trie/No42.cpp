785c9bba67e68e0bded31a0fbf1e30d1fbc4320d
Guho-Kim <weeeeek2@naver.com>
2023-08-05 22:13:15 +0900

    2023-08-05

--- Basic_Problem/12_Trie/No42.cpp
+++ Basic_Problem/12_Trie/No42.cpp
@@ -1,113 +1,117 @@
-// [SWEA] No 1256. [S/W 문제해결 응용] 6일차 - K번째 접미어
-
-#include<iostream>
-#include<cstring>
-#include<vector>
+#include <iostream>
+#include <cstring>
+#include <vector>
 using namespace std;
+
 vector<char> vec;
 
 class Trie {
-	static constexpr size_t M = 26;
-	static constexpr char OFFSET = 'a';
+    static constexpr size_t M = 26;
+    static constexpr char OFFSET = 'a';
 
-	struct TrieNode {
-		int child[M];
-		bool is_terminal;
+    struct TrieNode {
+        int child[M];
+        bool is_terminal;
 
-		TrieNode() {
-			memset(child, -1, sizeof(int) * M);
-			is_terminal = false;
-		}
-	};
+        TrieNode() {
+            memset(child, -1, sizeof(int) * M);
+            is_terminal = false;
+        }
+    };
 
-	vector< TrieNode > nodes;
+    vector<TrieNode> nodes;
 
 public:
-	Trie() : nodes(1) {}
+    Trie() : nodes(1) {}
 
-	void init() {
-		nodes.resize(1);
-		nodes[0] = TrieNode();
-	}
+    void init() {
+        nodes.resize(1);
+        nodes[0] = TrieNode();
+    }
 
-	void insert(const string& str) {
+    void insert(const string& str) {
+        int node_id = 0;
+        for (const auto& c : str) {
+            if (nodes[node_id].child[c - OFFSET] == -1) {
+                nodes[node_id].child[c - OFFSET] = nodes.size();
+                nodes.emplace_back();
+            }
+            node_id = nodes[node_id].child[c - OFFSET];
+        }
+        nodes[node_id].is_terminal = true;
+    }
+
+    void remove(const string& str) {
+        int node_id = 0;
+        for (const auto& c : str) {
+            if (nodes[node_id].child[c - OFFSET] == -1) {
+                return;
+            }
+            node_id = nodes[node_id].child[c - OFFSET];
+        }
+        nodes[node_id].is_terminal = false;
+    }
+
+    bool find(const string& str) const {
+        int node_id = 0;
+        for (const auto& c : str) {
+            if (nodes[node_id].child[c - OFFSET] == -1) {
+                return false;
+            }
+            node_id = nodes[node_id].child[c - OFFSET];
+        }
+        return nodes[node_id].is_terminal;
+    }
+	string getKthWord(int K) {
 		int node_id = 0;
-		for (const auto& c : str) {
-			if (nodes[node_id].child[c - OFFSET] == -1) {
-				nodes[node_id].child[c - OFFSET] = nodes.size();
-				nodes.emplace_back();
-			}
-			node_id = nodes[node_id].child[c - OFFSET];
-		}
-		nodes[node_id].is_terminal = true;
+		string kthWord = "";
+		findKthWordDFS(node_id, "", K, kthWord);
+		return kthWord;
 	}
+	void findKthWordDFS(int node_id, string currentWord, int& K, string& kthWord) {
+		if (K == 0) return;
 
-	void remove(const string& str) {
-		int node_id = 0;
-		for (const auto& c : str) {
-			if (nodes[node_id].child[c - OFFSET] == -1) {
+		if (nodes[node_id].is_terminal) {
+			K--;
+			if (K == 0) {
+				kthWord = currentWord;
 				return;
 			}
-			node_id = nodes[node_id].child[c - OFFSET];
 		}
-		nodes[node_id].is_terminal = false;
-	}
 
-	bool find(const string& str) const {
-		int node_id = 0;
-		for (const auto& c : str) {
-			if (nodes[node_id].child[c - OFFSET] == -1) {
-				return false;
+		for (int i = 0; i < M; i++) {
+			int child_id = nodes[node_id].child[i];
+			if (child_id != -1) {
+				char ch = i + OFFSET;
+				findKthWordDFS(child_id, currentWord + ch, K, kthWord);
 			}
-			node_id = nodes[node_id].child[c - OFFSET];
 		}
-		return nodes[node_id].is_terminal;
 	}
 
-    string findKthSuffix(int K) {
-        int node_id = 0;
-        string kthSuffix;
-        while (K > 0) {
-            for (int i = 0; i < M; i++) {
-                int child_id = nodes[node_id].child[i];
-                if (child_id != -1) {
-                    kthSuffix.push_back(i + OFFSET);
-                    if (nodes[child_id].is_terminal) {
-                        K--;
-                        if (K == 0) break;
-                    }
-                    node_id = child_id;
-                    break;
-                }
-            }
-        }
-        return kthSuffix;
-    }
-    
 };
 
 Trie trie;
-void solve(){
-    int K; cin>>K;    
-    string str; cin>>str;
+
+void solve() {
+    int K;
+    cin >> K;
+    string str;
+    cin >> str;
 
     trie.init();
-    for(int i=str.size()-1; i>=0; i--){
-        trie.insert(str.substr(i));
-    }
-    string result;
-    cout<<trie.findKthSuffix(K);
+    for (int i = str.size() - 1; i >= 0; i--) trie.insert(str.substr(i));
+    cout << trie.getKthWord(K);
 }
 
-int main(int argc, char** argv)
-{
-    ios::sync_with_stdio(0);//cin.tie();
-	int test_case, T;cin>>T;
-   
-	for(test_case = 1; test_case <= T; ++test_case){
-        cout<<'#'<<test_case<<' ';
+int main(int argc, char** argv) {
+    ios::sync_with_stdio(0);  // cin.tie();
+    int test_case, T;
+    cin >> T;
+
+    for (test_case = 1; test_case <= T; ++test_case) {
+        cout << '#' << test_case << ' ';
         solve();
-        cout<<'\n';
-    	}
-	return 0;
+        cout << '\n';
+    }
+    return 0;
 }
